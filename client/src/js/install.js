08b78b2084019596065767f6f1c875c93282ca73
jwblau2 <jeremy@abundantly.com>
2023-11-20 18:51:45 -0700

    added to the install.js file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,34 @@
-const butInstall = document.getElementById('buttonInstall');
-
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+let deferredPrompt;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+  // Prevent the default installation prompt
+  event.preventDefault();
+  // Storing the event to use it later
+  deferredPrompt = event;
+  // Show your custom installation button or UI element
+  butInstall.style.display = 'block';
+});
+
+butInstall.addEventListener('click', async () => {
+  // Check if there is a deferredPrompt
+  if (deferredPrompt) {
+    // Show the installation prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const choiceResult = await deferredPrompt.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the install prompt');
+    } else {
+      console.log('User dismissed the install prompt');
+    }
+    // Clear the deferredPrompt variable as it's a one-time event
+    deferredPrompt = null;
+  }
+});
+
+window.addEventListener('appinstalled', (event) => {
+  // Perform actions after successful installation, if needed
+  console.log('App installed successfully!');
+});
